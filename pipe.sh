18a1ec3e6157736e9073d89ca562f0a82dfbe62c
Jan-Luca <sunsettrack4@gmail.com>
2018-04-28 14:52:00 +0200

    Fix possible issues in pipe scripts

--- pipe.sh
+++ pipe.sh
@@ -1,6 +1,6 @@
 session=$(<work/session)
-curl -i -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/x-www-form-urlencoded" -v --cookie "$session" --data "stream_type=hls&https_watch_urls=True&timeshift=10800" https://zattoo.com/zapi/watch/live/CID_CHANNEL | sed '/"url":/!d;s/.*"stream": {"url": "//g;s/", .*//g;' > work/chlink && PIPE=$(sed '/zahs/{s/zahs.tv.*/zahs.tv/g;s/\//\\\//g;}' work/chlink) && curl $(<work/chlink) | \
-# sed "3!d;s/.*/#\!\/bin\/bash\n\/usr\/bin\/ffmpeg -loglevel fatal -i $PIPE\/& -vcodec copy -acodec copy -f mpegts -tune zerolatency -preset normal pipe:1/g;" > chpipe.sh
-# sed "/live-1500/!d;s/.*/#\!\/bin\/bash\n\/usr\/bin\/ffmpeg -loglevel fatal -i $PIPE\/& -vcodec copy -acodec copy -f mpegts -tune zerolatency -preset normal pipe:1/g;" > chpipe.sh
-# sed "/live-600/!d;s/.*/#\!\/bin\/bash\n\/usr\/bin\/ffmpeg -loglevel fatal -i $PIPE\/& -vcodec copy -acodec copy -f mpegts -tune zerolatency -preset normal pipe:1/g;" > chpipe.sh
+curl -i -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/x-www-form-urlencoded" --cookie "$session" --data "stream_type=hls&https_watch_urls=True&timeshift=10800" https://zattoo.com/zapi/watch/live/CID_CHANNEL | grep "{" | sed 's/.*"stream": {"url": "//g;s/", .*//g;' > work/chlink && PIPE=$(sed '/zahs/{s/https:/http:/g;s/zahs.tv.*/zahs.tv/g;s/\//\\\//g;}' work/chlink) && curl -s $(<work/chlink) | \
+# grep -E "live-5000|live-3000|live-1500" | sed "2,3d;s/.*/#\!\/bin\/bash\n\/usr\/bin\/ffmpeg -loglevel fatal -i $PIPE\/& -vcodec copy -acodec copy -f mpegts -tune zerolatency -preset normal pipe:1/g;" > chpipe.sh
+# grep "live-1500" | sed "s/.*/#\!\/bin\/bash\n\/usr\/bin\/ffmpeg -loglevel fatal -i $PIPE\/& -vcodec copy -acodec copy -f mpegts -tune zerolatency -preset normal pipe:1/g;" > chpipe.sh
+# grep "live-600" | sed "s/.*/#\!\/bin\/bash\n\/usr\/bin\/ffmpeg -loglevel fatal -i $PIPE\/& -vcodec copy -acodec copy -f mpegts -tune zerolatency -preset normal pipe:1/g;" > chpipe.sh
 bash chpipe.sh
